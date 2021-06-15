c5824152f011624c6ee6c701ae6fe9e04469f9d5
Franek <poczta.f@protonmail.com>
2021-06-15 11:13:40 +0200

    feat: Prepopulate database with dummy data

--- app/src/main/java/pl/skaucieuropy/rozliczwyjazd/database/ReckoningDatabase.kt
+++ app/src/main/java/pl/skaucieuropy/rozliczwyjazd/database/ReckoningDatabase.kt
@@ -2,8 +2,11 @@ package pl.skaucieuropy.rozliczwyjazd.database
 
 import android.content.Context
 import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteDatabase
 import pl.skaucieuropy.rozliczwyjazd.models.Camp
 import pl.skaucieuropy.rozliczwyjazd.models.Document
+import java.util.*
+import java.util.concurrent.Executors
 
 @Database(entities = [Document::class, Camp::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
@@ -27,6 +30,18 @@ abstract class ReckoningDatabase : RoomDatabase() {
                         "reckoning_database"
                     )
                         .fallbackToDestructiveMigration()
+                        .addCallback(object : RoomDatabase.Callback() {
+                            override fun onCreate(db: SupportSQLiteDatabase) {
+                                super.onCreate(db)
+                                Executors.newSingleThreadExecutor().execute {
+                                    instance?.let{
+                                        it.campDao.insertCamp(Camp(1, "Marydół 2021", 2000.0, Date(), Date(), true))
+                                        it.documentDao.insertDocument(Document(1, "1981/11/2019", "Faktura", Date(), "Art. spożywcze", 24.54, "", false, 1))
+                                        it.documentDao.insertDocument(Document(2, "1783F01107/12/19", "Faktura", Date(), "Art. przemysłowe", 382.99, "", false, 1))
+                                    }
+                                }
+                            }
+                        })
                         .build()
 
                     INSTANCE = instance
